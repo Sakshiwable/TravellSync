ce8ce8d38ccf61ca1e285551630023f063e9cc1c
atharvjadhav215 <168112689+atharvjadhav215@users.noreply.github.com>
2025-12-07 14:09:58 +0530

    Created Porject

--- server/server.js
+++ server/server.js
@@ -7,18 +7,13 @@ import { initSocket } from "./sockets/index.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
-// 🧩 Step 1: Connect to MongoDB
-connectDB();
 
-// 🧩 Step 2: Create an HTTP server from Express app
 const server = http.createServer(app);
 
-// 🧩 Step 3: Initialize WebSocket (real-time)
 initSocket(server);
 
-// 🧩 Step 4: Start the server
 server.listen(PORT, () => {
   console.log(`✅ Server running on port ${PORT}`);
 });
